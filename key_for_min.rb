81783663f1eea05330a9ec5893a4ef5608c72471
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-15 04:12:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,12 @@
 
 def key_for_min_value(name_hash)
 min_value = 0
-empty_hash = nil 
+empty_hash = nil
 name_hash.each do |key, value|
+  if lowest_value == 0 || value < lowest_value
+    lowest_value = value
+    lowest_key = key
+  end
 end
+return empty_hash
+end 
